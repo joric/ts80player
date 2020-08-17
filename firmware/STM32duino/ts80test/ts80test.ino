a49f19778b630b51bb33e8856af9511eb9fde4a0
joric <joric@users.noreply.github.com>
2020-08-17 20:05:06 +0500

    added beeper music

--- firmware/STM32duino/ts80test/ts80test.ino
+++ firmware/STM32duino/ts80test/ts80test.ino
@@ -1,15 +1,15 @@
-/*  
- Basic Arduino music player for TS80 (it works!) also works for Bluepill
- Install stm32duino as here https://github.com/stm32duino/Arduino_Core_STM32
- Select Board: "Generic STM32F1 Series"
- Board part number: "Generic F103T8"
- Upload method: "Maple DFU Bootloader 2.0"
- Edit %LOCALAPPDATA%\Arduino15\packages\STM32\hardware\stm32\1.9.0\platform.txt:
- build.preferred_out_format=hex
- Edit %LOCALAPPDATA%\Arduino15\packages\STM32\hardware\stm32\1.9.0\boards.txt:
- GenF1.menu.upload_method.dfu2Method.build.flash_offset=0x4000
- Use Sketch->Export Compiled binary to save hex in the sketch folder
- Upload via DFU bootloader, make sure it renames to RDY, power cycle the iron
+/*
+  Basic Arduino music player for TS80 (it works!) also works for Bluepill
+  Install stm32duino as here https://github.com/stm32duino/Arduino_Core_STM32
+  Select Board: "Generic STM32F1 Series"
+  Board part number: "Generic F103T8"
+  Upload method: "Maple DFU Bootloader 2.0"
+  Edit %LOCALAPPDATA%\Arduino15\packages\STM32\hardware\stm32\1.9.0\platform.txt:
+  build.preferred_out_format=hex
+  Edit %LOCALAPPDATA%\Arduino15\packages\STM32\hardware\stm32\1.9.0\boards.txt:
+  GenF1.menu.upload_method.dfu2Method.build.flash_offset=0x4000
+  Use Sketch->Export Compiled binary to save hex in the sketch folder
+  Upload via DFU bootloader, make sure it renames to RDY, power cycle the iron
 */
 
 #define LEDPIN     PA6 // audio output (TS80 heater pin)
@@ -45,9 +45,44 @@ void setup() {
   
 }
 
-void loop() {
+void beep(int freq, int ms) {
+  freq *=2; // freq fix for two poles  
   static uint8_t flag;
-  digitalWrite(LEDPIN, flag ? HIGH : LOW);
-  flag ^= 1;
-  delay(50);
+  unsigned long delta = 1000000 / freq;
+  unsigned long start = micros();
+  unsigned long end = start + ms * 1000;
+
+  while (micros() < end) {
+    unsigned long next = micros() + delta;
+    digitalWrite(LEDPIN, flag ? HIGH : LOW);
+    flag ^= 1;
+    while (micros() < next) {
+      delayMicroseconds(1);      
+    }
+  }  
+}
+
+#include "beeper_music.h"
+
+//#undef melody
+//#undef tempo
+//#define melody wish_melody
+//#define tempo wish_tempo
+//#define melody santa_melody
+//#define tempo santa_tempo
+
+void loop() {
+  /*
+  beep(220, 500);
+  beep(440, 500);
+  beep(880, 500);
+  */
+  int notes = sizeof(melody)/sizeof(melody[0]);
+  for (int i = 0; i<notes; i++) {
+    int freq = melody[i];
+    int ms = 1000/tempo[i];
+    beep(freq, ms);
+    int pauseBetweenNotes = ms * 0.1;
+    delay(pauseBetweenNotes);    
+  }  
 }
