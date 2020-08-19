65a9568256eb0371d6906edc93684f71cd68d46c
joric <joric@users.noreply.github.com>
2020-08-19 10:35:14 +0500

    chello release

--- firmware/STM32duino/ts80midi/ts80midi.ino
+++ firmware/STM32duino/ts80midi/ts80midi.ino
@@ -17,7 +17,6 @@ extern "C" void SystemClock_Config(void) {
 
 void setup() {
   pinMode(LEDPIN, OUTPUT);
-
 #if DISPLAY_ENABLED
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
@@ -26,9 +25,8 @@ void setup() {
   display.setCursor(0,0);
   display.setCursor(0,16);
   display.print("Playing...");
-  display.display();  
+  display.display();
 #endif
-  
 }
 
 void beep(int freq, int ms) {  
@@ -47,19 +45,34 @@ void beep(int freq, int ms) {
   }  
 }
 
+void update(int current, int total) {
+#if DISPLAY_ENABLED
+  display.clearDisplay();
+  display.setCursor(0,16);
+  display.print("Note: ");
+  display.print( current);
+  display.print("/");
+  display.print(total);
+  display.display();
+#endif
+}
 
-#include "bumble.h"
+
+
+//#include "bumble.h"
+#include "bach.h"
 
 void loop() {
-  const byte * p = bumble_data;
+  const byte * p = bach_data;
+  int notes = (sizeof(bach_data)-2)/3;
   byte rate = *p++;
   for (int i=0; *p; i++) {
       byte w = *p++;
       byte l = *p++;
       byte h = *p++;
-      int freq = 65536 * 64 / (h * 256 + l);
-      int ms = w * 10;
+      int freq = 65536 * 128 / (h * 256 + l);
+      int ms = w * 3;
       beep(freq, ms);
-      delay(ms*0.1);
+      update(i, notes);
   }
 }
